42fbc2885b8f7ee588511676059e21a8c2a57eba
tgavrel <tomas.gavrel@hotmail.fr>
2022-06-22 20:39:04 +0200

    Updated styles 🎨

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="row">
-    <div class="col-7">
-      <img class="w-100" @if(!isset($product->picture)) src="{{ asset('storage/image_default.png') }}" @else src="{{asset('storage/images/'.$product->picture)}}" @endif alt="image_produit">
+<div class="row my-5">
+    <div class="col-4 text-center">
+      <img class="shadow rounded" style="max-height:500px" @if(!isset($product->picture)) src="{{ asset('storage/image_default.png') }}" @else src="{{asset('storage/images/'.$product->picture)}}" @endif alt="image_produit">
     </div>
-    <div class="col-5">
+    <div class="col-8 bg-light rounded">
       <p class="fs-5">{{$product->name}}</p>
       <p>Description du produit : {{$product->description}}</p>
       <p>Prix : {{$product->price}}</p>
